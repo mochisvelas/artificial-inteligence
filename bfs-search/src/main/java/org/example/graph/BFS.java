01a848bfbbc29d346c657ba43aee0f61f2f8fdeb
mochisvelas <velasquezmochis@gmail.com>
2021-02-08 00:50:35 -0600

    create bfs class and algorithm

--- bfs-search/src/main/java/org/example/graph/BFS.java
+++ bfs-search/src/main/java/org/example/graph/BFS.java
@@ -1,5 +1,30 @@
 package org.example.graph;
 
+import java.util.*;
+
 public class BFS {
+
+    public static<T> Optional<Node<T>> search(T value, Node<T> start){
+        Queue<Node<T>> queue = new ArrayDeque<>();
+        queue.add(start);
+        Set<Node<T>> closed = new HashSet<>();
+        Node<T> currentNode;
+
+        while(!queue.isEmpty()){
+            currentNode = queue.remove();
+            System.out.println("Current node is "+ currentNode.getValue());
+
+            if(currentNode.getValue().equals(value)){
+                return Optional.of(currentNode);
+            }else{
+                closed.add(currentNode);
+                queue.addAll(currentNode.getNeighbors());
+                queue.removeAll(closed);
+
+            } 
+        }
+
+        return Optional.empty();
+    }
 
 }
