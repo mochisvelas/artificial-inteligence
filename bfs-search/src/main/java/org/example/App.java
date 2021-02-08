584d39852c45b6994bfacd970e613a2185795162
mochisvelas <velasquezmochis@gmail.com>
2021-02-08 11:12:31 -0600

    change pom.xml

--- bfs-search/src/main/java/org/example/App.java
+++ bfs-search/src/main/java/org/example/App.java
@@ -1,19 +1,43 @@
 package org.example;
 
 import org.example.graph.Node;
+import org.example.graph.BFS;
 
 public class App 
 {
     public static void main( String[] args )
     {
 
-        Node<String> node1 = new Node<>("First node");
+        Node<Integer> zona1 = new Node<>(1);
+        Node<Integer> zona2 = new Node<>(2);
+        Node<Integer> zona4 = new Node<>(4);
+        Node<Integer> zona5 = new Node<>(5);
+        Node<Integer> zona9 = new Node<>(9);
+        Node<Integer> zona10 = new Node<>(10);
+        Node<Integer> zona12 = new Node<>(12);
+        Node<Integer> zona13 = new Node<>(13);
+        Node<Integer> zona14 = new Node<>(14);
+        Node<Integer> zona15 = new Node<>(15);
+        Node<Integer> zona16 = new Node<>(16);
+        Node<Integer> zona21 = new Node<>(21);
 
-        Node<Boolean> node2 = new Node<>(true);
+        zona1.connect(zona2);
+        zona1.connect(zona4);
+        zona1.connect(zona5);
+        zona1.connect(zona9);
+        zona4.connect(zona5);
+        zona4.connect(zona9);
+        zona9.connect(zona10);
+        zona9.connect(zona13);
+        zona10.connect(zona15);
+        zona12.connect(zona13);
+        zona12.connect(zona21);
+        zona13.connect(zona14);
+        zona14.connect(zona10);
+        zona16.connect(zona10);
 
-        var node3 = new Node<Integer>(7);
-        
-        System.out.println(node3);
+        BFS.search(16, zona21);
 
     }
+
 }
