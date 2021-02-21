e50bc93c5321a016cfc93407cb532427fd72dc7b
mochisvelas <velasquezmochis@gmail.com>
2021-02-21 14:30:49 -0600

    readd import

--- csp/src/main/java/gt/edu/url/CSP.java
+++ csp/src/main/java/gt/edu/url/CSP.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
 import java.util.Collections;
 
