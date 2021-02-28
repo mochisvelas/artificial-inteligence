7a41c8c89e8ebf036f8d1e75114c4c222b16fa3a
mochisvelas <velasquezmochis@gmail.com>
2021-02-28 17:05:23 -0600

    create project

---
+++ minimax/src/main/java/gt/edu/url/Minimax.java
@@ -0,0 +1,13 @@
+package gt.edu.url;
+
+/**
+ * Hello world!
+ *
+ */
+public class Minimax 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
