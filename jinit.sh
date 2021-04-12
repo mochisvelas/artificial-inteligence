88ee0cbaa2bca59b0c417fb687d5c2475f7ef716
mochisvelas <velasquezmochis@gmail.com>
2021-04-11 18:55:07 -0600

    prolly finished

--- jinit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Maven creation project script
-
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DgroupId=gt.edu.url
