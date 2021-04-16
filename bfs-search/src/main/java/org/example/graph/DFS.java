ec68f0634fa4968556d40012b48ff80e25a389d5
Brenner Hernandez <47000856+mochisvelas@users.noreply.github.com>
2021-04-15 20:56:50 -0600

    Revert "Lab4"

---
+++ bfs-search/src/main/java/org/example/graph/DFS.java
@@ -0,0 +1,59 @@
+package org.example.graph;
+
+import java.util.*;
+
+public class DFS {
+
+    // method to find goal node
+    public static<T> Stack<Node<T>> search(T value, Node<T> start){
+        Stack<Node<T>> fringe = new Stack<>();
+        fringe.add(start);
+        Set<Node<T>> closed = new HashSet<>();
+        Node<T> currentNode;
+        Stack<Node<T>> visitedNodes = new Stack<>();
+        Boolean found = false;
+
+        while(!fringe.isEmpty()){ // if there's still nodes to explore
+            currentNode = fringe.pop();
+            System.out.println("Current node is "+ currentNode.getValue());
+
+            if(!visitedNodes.contains(currentNode)){
+                visitedNodes.add(currentNode);
+            }
+
+            if(currentNode.getValue().equals(value)){ // if current node is goal, break while
+                found = true;
+                break; 
+
+            }else{ // if not expand fringe and ignore already visited
+                closed.add(currentNode);
+                fringe.addAll(currentNode.getNeighbors());
+                fringe.removeAll(closed);
+
+            } 
+        }
+
+        // stack to store route
+        Stack<Node<T>> route = new Stack<>();
+        // push goal node
+        if(found){
+            route.push(visitedNodes.pop());
+
+            while(!visitedNodes.isEmpty()){
+                currentNode = visitedNodes.pop();
+
+                // if last node pushed into route is neighbor to currentNode push currentNode
+                if(route.peek().getNeighbors().contains(currentNode)){
+
+                    if(!route.contains(currentNode)){
+                        route.push(currentNode);
+                    }
+                }
+            }
+        }
+
+        // return stack with route
+        return route;
+    }
+
+}
